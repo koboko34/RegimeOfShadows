2eddeb2d9f6987b7aa931222e43024a60ca9235c
Armand Yilinkou <66181314+koboko34@users.noreply.github.com>
2023-10-30 23:12:24 +0000

    Added basic UI

---
+++ Source/RegimeOfShadows/MyPlayerController.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerController.h"
+#include "Blueprint/UserWidget.h"
+
+void AMyPlayerController::BeginPlay()
+{
+	UUserWidget* HUD = CreateWidget(this, HUDClass, TEXT("HUD"));
+	HUD->AddToViewport();
+}
